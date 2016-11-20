c25f1f2eb9226c752385df689333c55cff6a51f1
lukaszkuczynski <kuczynskilukasz@gmail.com>
2016-11-20 19:47:42 +0100

    can add new spending

--- client/main.js
+++ client/main.js
@@ -9,6 +9,25 @@ Spendings = new Mongo.Collection('spendings');
 
 Template.spendingList.helpers({
   spending() {
-    return Spendings.find();
+    return Spendings.find({},{'sort' : {'createdAt':-1}});
   }
 });
+
+Template.body.events({
+  'submit .add-spending'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const amount = target.amount.value;
+
+    Spendings.insert({
+      amount : amount,
+      createdAt: new Date() 
+    });
+
+    target.amount.value = '';
+
+  }
+    
+});
+
